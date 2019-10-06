55b8e894c639ef44495d724eee8e1770c6a42f75
jagritjkh <32562337+jagritjkh@users.noreply.github.com>
2019-10-06 18:54:04 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
-void main() { runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("IPU Result Analyser"),
-          ) ,)
-      ),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IPU Results',
+      home: HomePage(),
     );
+  }
 }
