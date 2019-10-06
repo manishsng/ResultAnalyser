8c4569fabe995e1b8c1cdee4bdba8413afca88fa
jagritjkh <32562337+jagritjkh@users.noreply.github.com>
2019-10-06 18:54:31 +0530

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:resultanalyser/main.dart';
+import 'package:ipu_results/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
